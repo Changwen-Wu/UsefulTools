4ece670202c51f5fe0b1bfe36aa060b50bb30c97
ChangwenWu <cw.wu1103@gmail.com>
2025-12-04 11:18:51 +0800

    Add files via upload

--- Clinical_Match/example.m
+++ Clinical_Match/example.m
@@ -1,17 +1,18 @@
+clear;clc
 % --- 1. 筛选标准 (Inclusion Criteria) ---
 inclu_cri = {
     'AGE:[6,18]';    % 年龄必须 < 18 (开区间，不包含18)
     'GROUP:{1,2}'        % 确保只保留 1(ASD) 和 2(HC) 组
 };
 
-% --- 2. 匹配标准 (Matching Criteria) ---
-match_cri = {
-    'T:AGE~GROUP';     % 匹配年龄 (连续变量用 T)
-    % 'F:AGE~GROUP*SITE';
-    'X2:SEX~GROUP';
-    '*T:FIQ~GROUP';            % 匹配智商 (IQ可能有缺失值，所以加 * 号忽略空值)
-    '*X2:HANDEDNESS~GROUP'     % 匹配头动 (假设你的Excel里头动列名叫 func_mean_fd)
-};
+% % --- 2. 匹配标准 (Matching Criteria) ---
+% match_cri = {
+%     'T:AGE~GROUP';     % 匹配年龄 (连续变量用 T)
+%     % 'F:AGE~GROUP*SITE';
+%     'X2:SEX~GROUP';
+%     '*T:FIQ~GROUP';            % 匹配智商 (IQ可能有缺失值，所以加 * 号忽略空值)
+%     '*X2:HANDEDNESS~GROUP'     % 匹配头动 (假设你的Excel里头动列名叫 func_mean_fd)
+% };
 
 pheno_path = "E:\Dataset\ABIDE\ABIDE\ABIDE_Subjects_Info.csv";
 sheet_name = "ABIDE_Subjects_Info";
@@ -27,6 +28,7 @@ match_cri = {
     'X2:SEX~GROUP';
     '*T:FIQ~GROUP';            % 匹配智商 (IQ可能有缺失值，所以加 * 号忽略空值)
     '*X2:HANDEDNESS~GROUP'     % 匹配头动 (假设你的Excel里头动列名叫 func_mean_fd)
+    'X2:SITE~GROUP' % 组间站点平衡
 };
 
 [raw_final, p_stats, summary_str] = Clinical_Match(pheno_path, sheet_name, inclu_cri, match_cri, match_method, min_counts, site_col_name, dx_col_name, group_names);
